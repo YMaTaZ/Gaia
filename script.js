f1ea35f54de0135cd7261befdc9cac7f77a7f9cb
MaTaZ <ymataz979459@gmail.com>
2025-12-16 23:43:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const images = [
-  "./img1.png",
-  "./img2.png",
-  "./img3.png",
-  "./img4.png",
-  "./img5.png"
+  "img1.png",
+  "img2.png",
+  "img3.png",
+  "img4.png",
+  "img5.png"
 ];
 
 const texts = [
